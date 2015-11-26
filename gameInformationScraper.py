b326276ef6634786b3394058e33b7a0a87fd50be
= <=>
2015-11-26 19:25:42 +1100

    scrape scrape steamdb.info

--- gameInformationScraper.py
+++ gameInformationScraper.py
@@ -2,16 +2,44 @@
 
 import sys
 import re
-import urllib2
+import requests
+import sqlite3 as lite
+from bs4 import BeautifulSoup
+import lxml
+
 
 def main():
     # read games to be read
-    f = open("statesPlayers.csv")
+    f = open("gamesToScrape.csv")
     heading = f.readline()
-    ids = [ re.split(",", line.rstrip())[1] for line in f.readlines()]
-    ids = [re.sub('"','', steamid) for steamid in ids]
+    appids = [ re.split(",", line.rstrip())[1] for line in f.readlines()]
+    appids = [re.sub('"','', appid) for appid in appids]
+
+    appids = ["730"]
+
+    for appid in appids:
+        i = 0
+        url =  getInfoUrl(appid)
+        htmls = requests.get(url).text
+        soup = BeautifulSoup(htmls, 'lxml')
+        trs = soup.find_all('tr')
+        metaCriticScore = trs[49].children[1]
+        print meta_critic_score
+        for row in trs[:50]:
+            print ""
+            print "index = ", i
+            print row
+            i += 1
 
 
+def getInfoUrl(appid):
+    return "https://steamdb.info/app/" + appid + "/info/"
+
+def exists(connection, appid):
+    c = connection.cursor()
+    q = (str(appid),)
+    c.execute("SELECT EXISTS(SELECT 1 FROM Games WHERE appid=?);",q)
+    return 1 == c.fetchone()[0]
 
 if __name__ == '__main__':
     main()
--- makeDB/makeGameDB.py
+++ makeDB/makeGameDB.py
@@ -3,9 +3,13 @@ import sqlite3 as lite
 # makes the to be scraped database 
 con = lite.connect('playersGames.db')
 c = con.cursor()
-c.execute("""CREATE TABLE PlayersGames (
-    steamid TEXT,
-    gameCount TEXT
+c.execute("""CREATE TABLE Games (
+    appid TEXT,
+    name TEXT,
+    genres TEXT,
+    score INTEGER,
+    rating TEXT,
+    consumerAdvice TEXT
 );""")
 con.commit()
 con.close()
